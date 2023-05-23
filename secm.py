c2afaa4e9dff3ffb9c3e7ebed01519932ac37181
Henrik Spitzenpfeil <henrik.spitzenpfeil@gmail.com>
2023-05-23 09:10:24 +0200

    adjusted documentation

--- secm.py
+++ secm.py
@@ -2,10 +2,11 @@ import abc
 from abc import ABC
 from langpy import langpy
 from autolab import autolab
+import time
 
 class SECM():
     
-    '''A class representing the Scanning electrochemical microscope'''
+    """class representing the Scanning electrochemical microscope"""
 
     def __init__(self, potentiostat_config, stepper_config):
         self.potentiostat = autolab.potentiostat(potentiostat_config)
@@ -14,7 +15,6 @@ class SECM():
     def FindFeedbackHeight(self,
                            MaxWay: float,
                            StepLength: float,
-                           MaxSpeed: float,
                            StopCondition: float = 0.25
                            ) -> list:
         """Performs a line scan experiment along the z axis of the SECM.
@@ -23,23 +23,39 @@ class SECM():
         change of measured current from the reference current.
          Returns the approach curve data as nested list [[position, current]].
          """
-        
-        #TODO: find how to set speed of the LSTEP controller
 
-        self.MotorController.SetDistance(0, 0, -StepLength, 0)
-        #self.MotorController.SetSpeed(MaxSpeed)
+        while abs((current-reference_current)/reference_current) < StopCondition and way_traveled < MaxWay:
+            self.MotorController.MoveRelShort()
+            current = self.potentiostat.get_actual_values()[1]
+            way_traveled = way_traveled + StepLength
+            ApproachCurve.append([way_traveled, current])
+        return ApproachCurve
+    
+    def LineScan(self, MaxWay, StepLength, WaitTime, Direction) -> List:
+
+        """ Perform a line scan experiment. WaitTime in seconds.
+        Direction can be x, y or z."""
+
+        DirectionMap = {x: (StepLength, 0, 0, 0),
+                        y: (0,StepLength, 0, 0),
+                        z: (0, 0, StepLength, 0)
+                        }
+        self.MotorController.SetDistance(DirectionMap[Direction])
         way_traveled = 0
         self.potentiostat.cell_on()
         reference_current = self.potentiostat.get_actual_values()[1]
         current = reference_current
         ApproachCurve = [[way_traveled, current]]
 
-        while abs((current-reference_current)/reference_current)<StopCondition and way_traveled < MaxWay:
+        while way_traveled < MaxWay:
             self.MotorController.MoveRelShort()
+            time.sleep(WaitTime)
             current = self.potentiostat.get_actual_values()[1]
             way_traveled = way_traveled + StepLength
             ApproachCurve.append([way_traveled, current])
         return ApproachCurve
+
+
  
  #def MeasureApproach(self):
     #    self.MotorController.MoveRelShort()
