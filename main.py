8cb7f365ab8e0fa45b58b99eb3685e9ccce410d3
Henrik Spitzenpfeil <henrik.spitzenpfeil@gmail.com>
2023-05-24 14:15:58 +0200

    added more properties and dummy functions

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ class SECM():
     def __init__(self, potentiostat_config, stepper_config):
         self.potentiostat = autolab.potentiostat(potentiostat_config)
         self.motor_controller = langpy.LStepController(stepper_config)
-        self._measurement_spots = 0
+        self._measurement_spots = self.measurement_spots()
+        self._wash_position = self.wash_position
+        self._dip_position = self.dip_position
+        self._substrate_initial_spot = self.substrate_initial_spot
         self.electrode_size = 0
         self.substrate_size = [0, 0]
 
@@ -22,6 +25,30 @@ class SECM():
     
     @measurement_spots.setter
     def measurement_spots(self, amount: int) -> None:
+        ...
+
+    @property
+    def wash_position(self) -> int:
+        return self._wash_position
+    
+    @wash_position.setter
+    def wash_position(self, amount: int) -> None:
+        ...
+    
+    @property
+    def dip_position(self) -> int:
+        return self._dip_position
+    
+    @dip_position.setter
+    def dip_position(self, amount: int) -> None:
+        ...
+
+    @property
+    def substrate_initial_spot(self) -> int:
+        return self._measurement_spots
+    
+    @substrate_initial_spot.setter
+    def substrate_initial_spot(self, amount: int) -> None:
         ...
 
     def total_measurement_spots(self) -> int:
@@ -41,10 +68,10 @@ class SECM():
        # self.find_feedback_height()
        # print('Substrate calibrated successfully')
 
-    def find_feedback_height(self,
+    """TODO: Implement force sensor of sdc. Async for contact?"""
+    def find_contact(self,
                            MaxWay: float,
                            StepLength: float,
-                           MaxSpeed: float,
                            StopCondition: float = 0.25
                            ) -> list:
         '''Performs a line scan experiment along the z axis of the SECM.
@@ -73,4 +100,16 @@ class SECM():
     
     #TODO: Implement this as a function of electrode size and substrate size
     def move_to_next_experiment(self):
+        ...
+    
+    def move_to_wash(self):
+        ...
+    
+    def move_to_dip(self):
+        ...
+    
+    def flush_cell(self):
+        ...
+    
+    def make_droplet(self):
         ...
