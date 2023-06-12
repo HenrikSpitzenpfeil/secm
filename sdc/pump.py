949a1ddc39feafc95043db34be8272a4407a8ed8
Henrik Spitzenpfeil <henrik.spitzenpfeil@gmail.com>
2023-06-12 11:33:54 +0200

    fixed pump implementation

--- sdc/pump.py
+++ sdc/pump.py
@@ -1,12 +1,15 @@
 import serial
-import json
-import os
+#import json
+#import os
 import re
 import time
-from config.definitions import ROOT_DIR
+
+
 class MicrodosePump:
+
     """Class for the Microdose PCON-C Pump that is used to move elctrolyte to 
     and from the SDC Head"""
+
     def __init__(self, config: dict) -> None:
         try:
             self.pump =  serial.Serial(config['port'],
@@ -16,6 +19,7 @@ class MicrodosePump:
         except:
             print("Failed to connect to Microdose Pump")
         
+        # Matches the handshake status pattern of the pump
         self.handshake_regex = re.compile("\d,HS\,[A-Z]{2}(?:\,?\d?)*")
     
     def set_program(self,
@@ -26,14 +30,18 @@ class MicrodosePump:
         
         """Set the Pump program which will be executed when run_pump is called.
         Direction is 0 to pump to the SDC Head, 1 to pump away."""
-
+        # Set the pump units to mikrolitre
         self.pump.write(bytes('1,WPU,1,0,0,1.0\r','utf-8'))
+        # Set program information to 1 cycle, 1 step, and program name
         self.pump.write(bytes('1,WPI,1,1,1,1,Default\r','utf-8'))
+        # Set pump to volume control and volume to be moved
         self.pump.write(bytes('1,WVT,1,1,0,{},Dispense\r'.format(volume),'utf-8'))
+        # Set the flowrate and direction of the program
         self.pump.write(bytes('1,WFR,1,1,{},{},{}\r'.format(flowrate,
                                                            flowrate,
                                                            direction),'utf-8'))
-        self.ser.write(bytes('1,WSC,1,1,0,0\r','utf-8'))
+        # Set the start signal to run the program immediately upon execution
+        self.pump.write(bytes('1,WSC,1,1,0,0\r','utf-8'))
         return None
     
     def run_pump(self) -> None:
@@ -43,21 +51,30 @@ class MicrodosePump:
         self.ensure_pump_status_ready() # ensures that pump program has completed
         return None
 
-    def ensure_pump_status_ready(self) -> None:
+    def ensure_pump_status_ready(self) -> bool:
+        """ Ensures that pump program has completed
+          and the pump is available to receive commands."""
         while True:
-            pump_status = self.get_pump_status
-            if pump_status == 1:
-                break
+            pump_status = int(self.get_pump_status()[8])
+            if pump_status == 1: # Pump status 1 is  pump in command mode available to receive commands
+                return True
             time.sleep(0.1)
     
-    def get_pump_status(self) -> int:
+    def get_pump_status(self) -> str:
         """Get the pump status code from the handshake. 
         Returns intiger status code."""
         
-        self.pump.write(bytes('1,RSS,0\r', 'utf-8'))
-        answer = self.pump.read(100)
-        handshake = re.search(self.handshake_regex, answer.decode())
-        return handshake[6]
+        self.pump.write(bytes('1,RSS,0\r', 'utf-8')) # command to ask for the pump status
+        answer = self.read_pump
+        
+        # iterate over the list of pump outputs to find the handshake with pump status information
+        for i in answer:
+            match = re.search(self.handshake_regex, i)
+            if match != None:
+                return match.group()
 
-    def read_pump(self) -> str:
-        return self.pump.read(1000)
+    def read_pump(self) -> list:
+        """ Reads the COM Buffer and returns a list of strings."""
+        out = self.pump.read(1000) 
+        out = out.decode() # Turns bytes into strings
+        return out.split('\r')
